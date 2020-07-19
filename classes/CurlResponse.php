4148b739369d68bad644b9b6f5a9d44bc74c92a8
rebal15 <lrebato10@gmail.com>
2020-07-19 16:46:56 +0100

    Added curl wrapper classes

---
+++ classes/CurlResponse.php
@@ -0,0 +1,101 @@
+<?php
+
+/**
+ Copyright (c) 2008 Sean Huber - shuber@huberry.com
+ 
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+ 
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+ 
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+**/
+
+/**
+ * Parses the response from a Curl request into an object containing
+ * the response body and an associative array of headers
+ *
+ * @package curl
+ * @author Sean Huber <shuber@huberry.com>
+**/
+class CurlResponse {
+    
+    /**
+     * The body of the response without the headers block
+     *
+     * @var string
+    **/
+    public $body = '';
+    
+    /**
+     * An associative array containing the response's headers
+     *
+     * @var array
+    **/
+    public $headers = array();
+    
+    /**
+     * Accepts the result of a curl request as a string
+     *
+     * <code>
+     * $response = new CurlResponse(curl_exec($curl_handle));
+     * echo $response->body;
+     * echo $response->headers['Status'];
+     * </code>
+     *
+     * @param string $response
+    **/
+    function __construct($response) {
+        # Headers regex
+        $pattern = '#HTTP/\d\.\d.*?$.*?\r\n\r\n#ims';
+        
+        # Extract headers from response
+        preg_match_all($pattern, $response, $matches);
+        $headers_string = array_pop($matches[0]);
+        $headers = explode("\r\n", str_replace("\r\n\r\n", '', $headers_string));
+        
+        # Remove headers from the response body
+        $this->body = str_replace($headers_string, '', $response);
+        
+        # Extract the version and status from the first header
+        $version_and_status = array_shift($headers);
+        preg_match('#HTTP/(\d\.\d)\s(\d\d\d)\s(.*)#', $version_and_status, $matches);
+        $this->headers['Http-Version'] = $matches[1];
+        $this->headers['Status-Code'] = $matches[2];
+        $this->headers['Status'] = $matches[2].' '.$matches[3];
+        
+        # Convert headers into an associative array
+        foreach ($headers as $header) {
+            preg_match('#(.*?)\:\s(.*)#', $header, $matches);
+            $this->headers[$matches[1]] = $matches[2];
+        }
+    }
+    
+    /**
+     * Returns the response body
+     *
+     * <code>
+     * $curl = new Curl;
+     * $response = $curl->get('google.com');
+     * echo $response;  # => echo $response->body;
+     * </code>
+     *
+     * @return string
+    **/
+    function __toString() {
+        return $this->body;
+    }
+    
+}
