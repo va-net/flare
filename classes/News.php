16c42ef42e28ec1f5575108783b3b0ec6fedc51c
rebal15 <lrebato10@gmail.com>
2020-08-03 14:02:18 +0100

    Edit and delete news articles
    Added the ability to edit and delete existing news articles.

--- classes/News.php
+++ classes/News.php
@@ -17,13 +17,14 @@ class News
 
         self::init();
 
-        $result = self::$_db->get('news', array('id', '>', 0), array('dateposted', 'DESC'));
+        $result = self::$_db->get('news', array('status', '=', 1), array('dateposted', 'DESC'));
 
         $x = 0;
         $news = array();
 
         while ($x < $result->count()) {
             $newdata = array(
+                'id' => $result->results()[$x]->id,
                 'title' => $result->results()[$x]->subject,
                 'author' => $result->results()[$x]->author,
                 'content' => $result->results()[$x]->content,
@@ -33,6 +34,29 @@ class News
             $x++;
         }
         return $news;
+
+    }
+
+    public static function archive($id)
+    {
+
+        self::init();
+
+        self::$_db->update('news', $id, 'id', array(
+            'status' => 2
+        ));
+
+    }
+
+    public static function edit($id, $fields) 
+    {
+
+        self::init();
+
+        if (self::$_db->update('news', $id, 'id', $fields)) {
+            return true;
+        }
+        return false;
 
     }
 
