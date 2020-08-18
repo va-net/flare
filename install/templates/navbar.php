4c6132e7a510d6505344be05c05ee8352eb27b13
Kai MALCOLM <MAL0048@fhs.vic.edu.au>
2020-08-18 13:53:39 +1000

    Update navbar.php

--- install/templates/navbar.php
+++ install/templates/navbar.php
@@ -7,16 +7,4 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 ?>
-<a class="navbar-brand" href="#"><span>Install Flare</span></a>
-
-<!-- navbar toggler -->
-<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
-    <span class="navbar-toggler-icon"></span>
-</button>
-
-<!-- navbar links -->
-<div class="collapse navbar-collapse" id="collapsibleNavbar">
-    <ul class="navbar-nav">
-        
-    </ul>
-</div>
+<span class="navbar-brand">Install Flare</span>
