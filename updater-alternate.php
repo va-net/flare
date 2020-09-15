f9830b1c838266dea8a0ab94ff02eed26d16500e
Velocity23 <34199284+Velocity23@users.noreply.github.com>
2020-09-15 10:46:34 +1000

    Remove TODO

--- updater-alternate.php
+++ updater-alternate.php
@@ -21,8 +21,6 @@ if (!$user->hasPermission('opsmanage')) {
 
 $current = Updater::getVersion();
 
-// TODO: #140 Make this use Updater class and use CHECK_PRERELEASE from DB
-
 // Get Releases
 $opts = array(
     'http'=>array(
