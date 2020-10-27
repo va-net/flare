cdfff9d69bf500d7aad4c6bf4b94c659b2941eae
Velocity23 <34199284+Velocity23@users.noreply.github.com>
2020-10-28 09:59:45 +1100

    Fix Plugins Issue
    Seems like a Git Issue, fixed now

--- core/init.php
+++ core/init.php
@@ -32,6 +32,16 @@ require_once __DIR__.'/listeners.php';
 require_once __DIR__.'/../functions/escape.php';
 require_once __DIR__.'/../includes/menus.php';
 
+$slash = "/";
+if (strpos(strtolower(php_uname('s')), "window") !== FALSE) {
+    $slash = "\\";
+}
+$INSTALLED_PLUGINS = Json::decode(file_get_contents(__DIR__.$slash.'..'.$slash.'plugins.json'));
+foreach ($INSTALLED_PLUGINS as $p) {
+    $classname = $p["class"];
+    $classname::init();
+}
+
 if (file_exists('./config.php') && Config::get('mysql/host') != 'DB_HOST' && Cookie::exists(Config::get('remember/cookie_name')) && !Session::exists(Config::get('session/session_name'))) {
     $hash = Cookie::get(Config::get('remember/cookie_name'));
     $hashCheck = DB::getInstance()->get('sessions', array('hash', '=', $hash));
