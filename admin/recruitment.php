28fa92cec80cadca1a5c2ab8e19a99c1a3f1fcad
Velocity23 <34199284+Velocity23@users.noreply.github.com>
2020-12-25 09:02:40 +1100

    Update recruitment.php

--- admin/recruitment.php
+++ admin/recruitment.php
@@ -12,7 +12,7 @@ require_once '../core/init.php';
 $user = new User();
 Page::excludeAsset('chartjs');
 
-Page::setTitle('Recruitment Admin - '.Config::get('va/name'));
+Page::setTitle('Recruitment Admin - ' . Config::get('va/name'));
 
 if (!$user->isLoggedIn()) {
     Redirect::to('/index.php');
@@ -24,9 +24,11 @@ $ACTIVE_CATEGORY = 'user-management';
 ?>
 <!DOCTYPE html>
 <html>
+
 <head>
     <?php include '../includes/header.php'; ?>
 </head>
+
 <body>
     <nav class="navbar navbar-dark navbar-expand-lg bg-custom">
         <?php include '../includes/navbar.php'; ?>
@@ -36,18 +38,20 @@ $ACTIVE_CATEGORY = 'user-management';
             <div class="row m-0 p-0">
                 <?php include '../includes/sidebar.php'; ?>
                 <div class="col-lg-9 main-content">
-                    <div id="loader-wrapper"><div id="loader" class="spinner-border spinner-border-sm spinner-custom"></div></div>
+                    <div id="loader-wrapper">
+                        <div id="loader" class="spinner-border spinner-border-sm spinner-custom"></div>
+                    </div>
                     <div class="loaded">
                         <?php
-                        if (file_exists(__DIR__.'/../install/install.php') && !file_exists(__DIR__.'/../.development')) {
+                        if (file_exists(__DIR__ . '/../install/install.php') && !file_exists(__DIR__ . '/../.development')) {
                             echo '<div class="alert alert-danger text-center">The Install Folder still Exists! Please delete it immediately, it poses a severe security risk.</div>';
                         }
-                        
+
                         if (Session::exists('error')) {
-                            echo '<div class="alert alert-danger text-center">Error: '.Session::flash('error').'</div>';
+                            echo '<div class="alert alert-danger text-center">Error: ' . Session::flash('error') . '</div>';
                         }
                         if (Session::exists('success')) {
-                            echo '<div class="alert alert-success text-center">'.Session::flash('success').'</div>';
+                            echo '<div class="alert alert-success text-center">' . Session::flash('success') . '</div>';
                         }
                         ?>
                         <h3>Recruitment</h3>
@@ -87,19 +91,19 @@ $ACTIVE_CATEGORY = 'user-management';
                                     echo $user["email"];
                                     echo '</td><td class="mobile-hidden align-middle">';
                                     $username = explode('/', $user['ifc'])[4];
-                                    echo '<a href="'.$user['ifc'].'" target="_blank">'.$username.'</a>';
+                                    echo '<a href="' . $user['ifc'] . '" target="_blank">' . $username . '</a>';
                                     echo '</td><td class="align-middle">';
                                     if (array_key_exists(strtolower($username), $blacklist)) {
-                                        echo '<span class="badge badge-danger" data-toggle="tooltip" title="'.$blacklist[strtolower($username)].'">Blacklisted</span>';
+                                        echo '<span class="badge badge-danger" data-toggle="tooltip" title="' . $blacklist[strtolower($username)] . '">Blacklisted</span>';
                                     } elseif (array_key_exists(strtolower($username), $watchlist)) {
-                                        echo '<span class="badge badge-warning" data-toggle="tooltip" title="'.$watchlist[strtolower($username)].'">Watchlisted</span>';
+                                        echo '<span class="badge badge-warning" data-toggle="tooltip" title="' . $watchlist[strtolower($username)] . '">Watchlisted</span>';
                                     } else {
                                         echo '<span class="badge badge-success">None</span>';
                                     }
                                     echo '</td><td class="align-middle">&nbsp;';
-                                    if (!array_key_exists(strtolower($username), $blacklist)) echo '<button class="btn btn-success text-light" value="'.$user['id'].'" form="accept" type="submit" name="accept"><i class="fa fa-check"></i></button>&nbsp;';
-                                    echo '<button value="'.$user['id'].'" id="delconfirmbtn" data-toggle="modal" data-target="#user'.$x.'declinemodal" class="btn btn-danger text-light" name="decline"><i class="fa fa-times"></i></button>&nbsp;';
-                                    echo '<button id="delconfirmbtn" class="btn btn-primary text-light" data-toggle="modal" data-target="#user'.$x.'modal"><i class="fa fa-plus"></i></button>';
+                                    if (!array_key_exists(strtolower($username), $blacklist)) echo '<button class="btn btn-success text-light" value="' . $user['id'] . '" form="accept" type="submit" name="accept"><i class="fa fa-check"></i></button>&nbsp;';
+                                    echo '<button value="' . $user['id'] . '" id="delconfirmbtn" data-toggle="modal" data-target="#user' . $x . 'declinemodal" class="btn btn-danger text-light" name="decline"><i class="fa fa-times"></i></button>&nbsp;';
+                                    echo '<button id="delconfirmbtn" class="btn btn-primary text-light" data-toggle="modal" data-target="#user' . $x . 'modal"><i class="fa fa-plus"></i></button>';
                                     echo '</td>';
                                     $x++;
                                 }
@@ -115,7 +119,7 @@ $ACTIVE_CATEGORY = 'user-management';
                                         <div class="modal-header">
                                             <h5 class="modal-title" id="usermodal-title"></h5>
                                             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                            <span aria-hidden="true">&times;</span>
+                                                <span aria-hidden="true">&times;</span>
                                             </button>
                                         </div>
                                         <div class="modal-body">
@@ -159,13 +163,13 @@ $ACTIVE_CATEGORY = 'user-management';
                                         <div class="modal-header">
                                             <h5 class="modal-title" id="declinemodal-title">Decline Application</h5>
                                             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                            <span aria-hidden="true">&times;</span>
+                                                <span aria-hidden="true">&times;</span>
                                             </button>
                                         </div>
                                         <div class="modal-body">
                                             <form action="/update.php" method="post" id="declinemodal">
                                                 <input hidden class="form-control" name="action" value="declineapplication">
-                                                <input hidden class="form-control" name="id" value="'.$user['id'].'">
+                                                <input hidden class="form-control" name="id" value="<?= $user['id'] ?>">
                                                 <div class="form-group">
                                                     <label for="declinemodal-status">Reason for decline of application</label>
                                                     <input required type="text" class="form-control" name="declinereason">
@@ -173,13 +177,14 @@ $ACTIVE_CATEGORY = 'user-management';
                                             </form>
                                         </div>
                                         <div class="modal-footer">
-                                            <button class="btn bg-custom" form="declinemodal" type="submit">Decline</button>
+                                            <button class="btn btn-danger" form="declinemodal" type="submit">Decline</button>
                                             <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                        <?php $x++; } ?>
+                        <?php $x++;
+                        } ?>
                     </div>
                 </div>
             </div>
@@ -194,4 +199,5 @@ $ACTIVE_CATEGORY = 'user-management';
         });
     </script>
 </body>
+
 </html>
