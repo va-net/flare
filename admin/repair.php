a518ee81576f0a01fb249091b78bf28a6e41f606
Velocity23 <34199284+Velocity23@users.noreply.github.com>
2021-01-03 16:33:14 +1100

    Update repair.php

--- admin/repair.php
+++ admin/repair.php
@@ -5,3 +5,7 @@ $test = $db->query("SELECT `notes` FROM `routes`");
 if ($test->error()) {
     $db->query("ALTER TABLE `routes` ADD `notes` TEXT NULL DEFAULT NULL");
 }
+
+if (Config::get('CHECK_PRERELEASE') == 0 && Updater::getVersion()["prerelease"]) {
+    Config::replace('CHECK_PRERELEASE', 1);
+}
