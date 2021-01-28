f7e65556f804a58d21f73ba900a36bd2c50c1a9f
Velocity23 <34199284+Velocity23@users.noreply.github.com>
2021-01-28 13:31:50 +1100

    Add TODOs to Routes

--- index.php
+++ index.php
@@ -30,41 +30,41 @@ Router::methodNotAllowed(function () {
 });
 
 // Pilot Pages
-Router::add('(/login)?', [new AuthController, 'get']);
-Router::add('(/login)?', [new AuthController, 'post'], 'post');
-Router::add('/apply', [new AuthController, 'apply_get']);
-Router::add('/apply', [new AuthController, 'apply_post'], 'post');
-Router::add('/logout', [new AuthController, 'logout']);
-Router::add('/home', [new HomeController, 'get']);
-Router::add('/home', [new HomeController, 'post'], 'post');
-Router::add('/pireps', [new stdClass, 'get']);
-Router::add('/pireps/([0-9]+)', [new stdClass, 'get']);
-Router::add('/pireps/new', [new stdClass, 'get']);
-Router::add('/pireps/new', [new stdClass, 'post'], 'post');
-Router::add('/routes', [new stdClass, 'get']);
-Router::add('/routes/([0-9]+)', [new stdClass, 'get']);
-Router::add('/map', [new stdClass, 'get']);
-Router::add('/events', [new stdClass, 'get']);
+Router::add('(/login)?', [new AuthController, 'get']); // [X]
+Router::add('(/login)?', [new AuthController, 'post'], 'post'); // [X]
+Router::add('/apply', [new AuthController, 'apply_get']); // [X]
+Router::add('/apply', [new AuthController, 'apply_post'], 'post'); // [X]
+Router::add('/logout', [new AuthController, 'logout']); // [X]
+Router::add('/home', [new HomeController, 'get']); // [X]
+Router::add('/home', [new HomeController, 'post'], 'post'); // [X]
+Router::add('/pireps', [new PirepsController, 'get_all']); // [X]
+Router::add('/pireps/([0-9]+)', [new stdClass, 'get']); // [ ]
+Router::add('/pireps/new', [new PirepsController, 'get_new']); // [X]
+Router::add('/pireps/new', [new PirepsController, 'post_new'], 'post'); // [X]
+Router::add('/routes', [new stdClass, 'get']); // [ ]
+Router::add('/routes/([0-9]+)', [new stdClass, 'get']); // [ ]
+Router::add('/map', [new stdClass, 'get']); // [ ]
+Router::add('/events', [new stdClass, 'get']); // [ ] // [ ]
 Router::add('/events/([0-9a-zA-z]{8}-[0-9a-zA-z]{4}-[0-9a-zA-z]{4}-[0-9a-zA-z]{4}-[0-9a-zA-z]{12})', [new stdClass, 'get']);
-Router::add('/pireps/acars', [new stdClass, 'get']);
+Router::add('/pireps/acars', [new stdClass, 'get']); // [ ]
 
 // Admin Pages
-Router::add('/admin/operations/ranks', [new stdClass, 'get']);
-Router::add('/admin/operations/fleet', [new stdClass, 'get']);
-Router::add('/admin/operations/routes', [new stdClass, 'get']);
-Router::add('/admin/operations/codeshares', [new stdClass, 'get']);
-Router::add('/admin/operations/events', [new stdClass, 'get']);
-Router::add('/admin/users', [new stdClass, 'get']);
-Router::add('/admin/users/pending', [new stdClass, 'get']);
-Router::add('/admin/users/staff', [new stdClass, 'get']);
-Router::add('/admin/pireps', [new stdClass, 'get']);
-Router::add('/admin/pireps/pending', [new stdClass, 'get']);
-Router::add('/admin/pireps/multipliers', [new stdClass, 'get']);
-Router::add('/admin/stats', [new stdClass, 'get']);
-Router::add('/admin', [new stdClass, 'get']);
-Router::add('/admin/news', [new stdClass, 'get']);
-Router::add('/admin/settings', [new stdClass, 'get']);
-Router::add('/admin/plugins', [new stdClass, 'get']);
-Router::add('/admin/plugins/installed', [new stdClass, 'get']);
+Router::add('/admin/operations/ranks', [new stdClass, 'get']); // [ ]
+Router::add('/admin/operations/fleet', [new stdClass, 'get']); // [ ]
+Router::add('/admin/operations/routes', [new stdClass, 'get']); // [ ]
+Router::add('/admin/operations/codeshares', [new stdClass, 'get']); // [ ]
+Router::add('/admin/operations/events', [new stdClass, 'get']); // [ ]
+Router::add('/admin/users', [new stdClass, 'get']); // [ ]
+Router::add('/admin/users/pending', [new stdClass, 'get']); // [ ]
+Router::add('/admin/users/staff', [new stdClass, 'get']); // [ ]
+Router::add('/admin/pireps', [new stdClass, 'get']); // [ ]
+Router::add('/admin/pireps/pending', [new stdClass, 'get']); // [ ]
+Router::add('/admin/pireps/multipliers', [new stdClass, 'get']); // [ ]
+Router::add('/admin/stats', [new stdClass, 'get']); // [ ]
+Router::add('/admin', [new stdClass, 'get']); // [ ]
+Router::add('/admin/news', [new stdClass, 'get']); // [ ]
+Router::add('/admin/settings', [new stdClass, 'get']); // [ ]
+Router::add('/admin/plugins', [new stdClass, 'get']); // [ ]
+Router::add('/admin/plugins/installed', [new stdClass, 'get']); // [ ]
 
 Router::run();
