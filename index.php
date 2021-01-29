8cebad11467141e4634daabe50ce22ac964f79d8
Velocity23 <34199284+Velocity23@users.noreply.github.com>
2021-01-29 23:03:33 +1100

    Update index.php

--- index.php
+++ index.php
@@ -13,11 +13,6 @@ require_once __DIR__ . '/core/init.php';
 $theme = Config::get('ACTIVE_THEME');
 if (empty($theme)) $theme = 'default';
 
-$initfile = __DIR__ . "/themes/{$theme}/_init.php";
-if (file_exists($initfile)) {
-    include($initfile);
-}
-
 Router::pathNotFound(function () {
     http_response_code(404);
     echo '<h1>Not Found</h1>';
@@ -43,13 +38,13 @@ Router::add('/pireps/new', [new PirepsController, 'get_new']);
 Router::add('/pireps/new', [new PirepsController, 'post_new'], 'post');
 Router::add('/pireps/setup', [new PirepsController, 'get_setup']);
 Router::add('/pireps/setup', [new PirepsController, 'post_setup'], 'post');
+Router::add('/pireps/acars', [new stdClass, 'get']); // [ ]
 Router::add('/routes', [new RoutesController, 'get']);
 Router::add('/routes/search', [new RoutesController, 'search']);
 Router::add('/routes/([0-9]+)', [new RoutesController, 'view']);
 Router::add('/map', [new MapController, 'get']);
 Router::add('/events', [new stdClass, 'get']); // [ ]
 Router::add('/events/([0-9a-zA-z]{8}-[0-9a-zA-z]{4}-[0-9a-zA-z]{4}-[0-9a-zA-z]{4}-[0-9a-zA-z]{12})', [new stdClass, 'get']); // [ ]
-Router::add('/pireps/acars', [new stdClass, 'get']); // [ ]
 
 // Admin Pages
 Router::add('/admin/operations/ranks', [new stdClass, 'get']); // [ ]
@@ -69,5 +64,10 @@ Router::add('/admin/news', [new stdClass, 'get']); // [ ]
 Router::add('/admin/settings', [new stdClass, 'get']); // [ ]
 Router::add('/admin/plugins', [new stdClass, 'get']); // [ ]
 Router::add('/admin/plugins/installed', [new stdClass, 'get']); // [ ]
+
+$initfile = __DIR__ . "/themes/{$theme}/_init.php";
+if (file_exists($initfile)) {
+    include($initfile);
+}
 
 Router::run();
