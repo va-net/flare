390e6874dbd9fb8e5ea156b40d0a231d0f5dc22b
Velocity23 <34199284+Velocity23@users.noreply.github.com>
2021-02-18 20:40:02 +1100

    Update success.php

--- install/templates/success.php
+++ install/templates/success.php
@@ -9,21 +9,23 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 ?>
 <!DOCTYPE html>
 <html>
+
 <head>
     <?php include './templates/header.php'; ?>
 </head>
+
 <body>
     <style>
         #loader {
-        position: absolute;
-        left: 50%;
-        top: 50%;
-        z-index: 1;
-        width: 150px;
-        height: 150px;
-        margin: -75px 0 0 -75px;
-        width: 120px;
-        height: 120px;
+            position: absolute;
+            left: 50%;
+            top: 50%;
+            z-index: 1;
+            width: 150px;
+            height: 150px;
+            margin: -75px 0 0 -75px;
+            width: 120px;
+            height: 120px;
         }
     </style>
 
@@ -35,20 +37,21 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
         <div class="container-fluid mt-4 text-center" style="overflow: auto;">
             <div class="main-content">
                 <div id="loader" class="spinner-border spinner-border-sm spinner-custom"></div>
-                    <div class="tab-content" id="tc">
-                        <div class="tab-pane container active" id="home" style="display: none;">
-                            <h3>Success!</h3>
-                            <p>Yay! Flare has been setup correctly. You may now <a href="../index.php">login</a> with the credentials you entered earlier to setup your staff members, permissions, and other handy features that Flare has to offer. Good luck!</p>
-                            <br>
-                            <p>If you need any help with anything, be sure to visit the <a href="https://vanet.app/tutorials">tutorials</a>.</p>
-                        </div>
+                <div class="tab-content" id="tc">
+                    <div class="tab-pane container active" id="home" style="display: none;">
+                        <h3>Success!</h3>
+                        <p>Yay! Flare has been setup correctly. You may now <a href="/">login</a> with the credentials you entered earlier to setup your staff members, permissions, and other handy features that Flare has to offer. Good luck!</p>
+                        <br>
+                        <p>If you need any help with anything, be sure to visit the <a href="https://vanet.app/tutorials">tutorials</a>.</p>
                     </div>
                 </div>
             </div>
-            <footer class="container-fluid text-center">
-                <?php include './templates/footer.php'; ?>
-            </footer>
         </div>
+        <footer class="container-fluid text-center">
+            <?php include './templates/footer.php'; ?>
+        </footer>
+    </div>
     </div>
 </body>
+
 </html>
