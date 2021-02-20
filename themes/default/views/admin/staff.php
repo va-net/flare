1572fb1863ebcebda3a973b2e2622ee430a17c96
Velocity23 <34199284+Velocity23@users.noreply.github.com>
2021-02-21 10:25:00 +1100

    Update staff.php

--- themes/default/views/admin/staff.php
+++ themes/default/views/admin/staff.php
@@ -66,7 +66,6 @@ $ACTIVE_CATEGORY = 'user-management';
                                     echo $staff["email"];
                                     echo '</td><td class="align-middle">';
                                     echo '<button class="btn text-light btn-primary" data-toggle="modal" data-target="#staff' . $x . 'modal" data-callsign="' . $staff['callsign'] . '" data-name="' . $staff['name'] . '" data-email="' . $staff['email'] . '" data-ifc="' . $staff['ifc'] . '" data-joined="' . date_format(date_create($staff['joined']), 'Y-m-d') . '" data-status="' . $staff['status'] . '" data-id="' . $staff['id'] . '"><i class="fa fa-edit"></i></button>';
-                                    echo '&nbsp;<button id="delconfirmbtn" class="btn text-light btn-danger" data-toggle="modal" data-target="#delconfirmmodal" data-callsign="' . $staff['callsign'] . '"><i class="fa fa-trash"></i></button>';
                                     echo '</td>';
                                     $x++;
                                 }
