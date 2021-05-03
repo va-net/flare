709bd44bdeafcbe276f6f692835cb1264971cdcf
Velocity23 <34199284+velocity23@users.noreply.github.com>
2021-05-04 08:20:09 +1000

    Docker Improvements

--- Dockerfile
+++ Dockerfile
@@ -8,6 +8,8 @@ RUN a2enmod rewrite
 
 COPY . /var/www/html
 
+RUN rm -rf /var/www/html/install
+
 RUN chown www-data:www-data -R /var/www/html/*
 
 CMD php /var/www/html/core/docker_entrypoint.php && apache2-foreground
--- core/docker_entrypoint.php
+++ core/docker_entrypoint.php
@@ -22,19 +22,3 @@ sleep(2);
 if (count((new User)->getAllUsers()) == 0) {
     Permissions::giveAll(1);
 }
-
-function rrmdir($dir)
-{
-    if (is_dir($dir)) {
-        $objects = scandir($dir);
-        foreach ($objects as $object) {
-            if ($object != "." && $object != "..") {
-                if (is_dir($dir . DIRECTORY_SEPARATOR . $object) && !is_link($dir . "/" . $object))
-                    rrmdir($dir . DIRECTORY_SEPARATOR . $object);
-                else
-                    unlink($dir . DIRECTORY_SEPARATOR . $object);
-            }
-        }
-        rmdir($dir);
-    }
-}
