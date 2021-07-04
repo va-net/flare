58c7308ec6301558157f88293c824c4d9261e954
Velocity23 <34199284+velocity23@users.noreply.github.com>
2021-07-04 13:10:14 +1000

    Update RoutesController.php

--- classes/controllers/RoutesController.php
+++ classes/controllers/RoutesController.php
@@ -17,7 +17,7 @@ class RoutesController extends Controller
         $data->user = $user;
         $data->va_name = Config::get('va/name');
         $data->is_gold = VANet::isGold();
-        $data->aircraft = Aircraft::getAvailableAircraft($user->rank(null, true))->results();
+        $data->aircraft = $user->getAvailableAircraft();
         $this->render('route_form', $data);
     }
 
@@ -63,6 +63,11 @@ class RoutesController extends Controller
                 array_push($stmts, (Input::get('duration') + 1) * 3600);
             }
         }
+        if (count($searchwhere) == 0) {
+            Session::flash('error', 'You must select at least one filter');
+            $this->redirect('/routes');
+        }
+
         $query = 'SELECT routes.fltnum, routes.dep, routes.arr, routes.duration, routes.id, routes.notes FROM routes';
         $i = 0;
         foreach ($searchwhere as $cond) {
