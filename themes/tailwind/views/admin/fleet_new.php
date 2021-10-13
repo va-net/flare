74da48229b7e2c966c23c576e95beb7495d42a75
Kai Malcolm <34199284+velocity23@users.noreply.github.com>
2021-10-13 17:19:23 +1100

    Tailwind Aircraft Awards

--- themes/tailwind/views/admin/fleet_new.php
+++ themes/tailwind/views/admin/fleet_new.php
@@ -4,6 +4,14 @@ require_once __DIR__ . '/../../includes/header.php';
 ?>
 <div id="content" class="text-black dark:text-white m-5">
     <h1 class="mb-3 text-3xl font-bold">Add Aircraft</h1>
+    <div class="flex gap-3 mb-3 items-center p-2 rounded bg-blue-500 dark:bg-blue-600 border-primary text-primary-text text-sm">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 flex-none" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+        </svg>
+        <p class="flex-1">
+            Aircraft can now be assigned a minimum rank and/or an award that is required for a pilot to be able to fly it. If both options are selected, the pilot can have the award OR the minimum rank to fly the aircraft. If no options are selected, the aircraft is not available to pilots.
+        </p>
+    </div>
     <form method="post" class="space-y-2" id="add-aircraft" x-data="{ liveries: {} }">
         <div class="space-y-1">
             <label for="type">Aircraft Type</label>
@@ -24,10 +32,19 @@ require_once __DIR__ . '/../../includes/header.php';
         </div>
         <div class="space-y-1">
             <label for="rank">Minimum Rank</label>
-            <select required name="rank" id="rank" class="form-control">
-                <option value>Select</option>
+            <select name="rank" id="rank" class="form-control">
+                <option value>None</option>
                 <?php foreach (Page::$pageData->ranks as $rank) : ?>
                     <option value="<?= $rank->id ?>"><?= $rank->name ?></option>
+                <?php endforeach; ?>
+            </select>
+        </div>
+        <div class="space-y-1">
+            <label for="award">Award</label>
+            <select name="award" id="award" class="form-control">
+                <option value>None</option>
+                <?php foreach (Page::$pageData->awards as $award) : ?>
+                    <option value="<?= $award->id ?>"><?= $award->name ?></option>
                 <?php endforeach; ?>
             </select>
         </div>
